a8f2c7c07cfba769a073284f922cde1078e08826
marymcharg <72612312+marymcharg@users.noreply.github.com>
2020-12-25 23:08:44 +0000

    Update scrape_last_fm.py

--- data-sources/scrape_last_fm.py
+++ data-sources/scrape_last_fm.py
@@ -7,6 +7,8 @@ import csv
 def clean_name(name):
     if " FT " in name:
         name = name[:name.find(" FT ")]
+    if " FEAT " in name:
+        name = name[:name.find(" FEAT ")]
     if " FEATURING " in name:
         name = name[:name.find(" FEATURING ")]
     if "/" in name:
@@ -26,17 +28,20 @@ def find_artist(dataset):
     birthplaces = []
     gender = []
     for artist in names:
-        name = clean_name(artist)
-        if name not in found_artists:            
-            print("Getting " + name)
-            url = name.replace(" ", "+")
-            found_artists[name] = find_birthplace(url, found_artists, gender, found_genders)  
-            birthplaces.append(found_artists.get(name))
+        if artist is float:
+            break
         else:
-            birthplaces.append(found_artists.get(name))
-            full_url = "https://www.last.fm/music/" + url
-            find_gender(full_url, found_genders, gender, name)
-    add_to_csv(birthplaces, gender)
+            name = clean_name(str(artist))
+            url = name.replace(" ", "+")
+            if name not in found_artists:            
+                print("Getting " + name)
+                found_artists[name] = find_birthplace(url, found_artists, gender, found_genders)  
+                birthplaces.append(found_artists.get(name))
+            else:
+                birthplaces.append(found_artists.get(name))
+                full_url = "https://www.last.fm/music/" + url
+                find_gender(full_url, found_genders, gender, name)
+    add_to_csv(birthplaces, gender, dataset)
 
 
 def find_birthplace(name, found_artists, gender, found_genders):
@@ -54,7 +59,7 @@ def find_birthplace(name, found_artists, gender, found_genders):
         req = requests.get(url)
         req.raise_for_status()
         soup = bs4.BeautifulSoup(req.text,"lxml")
-        error = soup.find_all("div", class_= "col-sm-3 col-sm-pull-6 error-page-marvin")   
+        error = soup.find_all("div", class_= "col-sm-7 col-sm-push-4")
         if error == []:    
             try: 
                 data = soup.find_all("dd", class_="catalogue-metadata-description")
@@ -65,45 +70,49 @@ def find_birthplace(name, found_artists, gender, found_genders):
                 find_gender(url, found_genders, gender, name)
                 return "N/A"
         else:
-            try:
                 alt_search(name, found_artists, gender, found_genders)
-            except:
-                gender.append("N/A")
-                return "N/A"
     except:
         gender.append("N/A")
+        found_genders[name] = "N/A"
         return "N/A"
 
 
 def alt_search(name, found_artists, gender, found_genders):
-    new_name = name[:name.find(" & ")]
-    if new_name not in found_artists:
-        try:
-            url = "https://www.last.fm/music/" + new_name
-            req = requests.get(url)
-            req.raise_for_status()
-            soup = bs4.BeautifulSoup(req.text,"lxml")
-            error = soup.find_all("div", class_= "col-sm-3 col-sm-pull-6 error-page-marvin")   
-            if error == []:   
-                try:
-                    data = soup.find_all("dd", class_="catalogue-metadata-description")
-                    birthplace = data[1].string
-                    found_artists[new_name] = found_artists.pop(name)
-                    print("Replacing " + name + " with " + new_name)
-                    find_gender(url, found_genders, gender, name)
-                    return birthplace
-                except:
-                    find_gender(url, found_genders, gender, name)
+    if " & " in name:
+        new_name = name[:name.find(" & ")]
+        if new_name not in found_artists:
+            try:
+                url = "https://www.last.fm/music/" + new_name
+                req = requests.get(url)
+                req.raise_for_status()
+                soup = bs4.BeautifulSoup(req.text,"lxml")
+                error = soup.find_all("div", class_= "col-sm-3 col-sm-pull-6 error-page-marvin")   
+                if error == []:   
+                    try:
+                        data = soup.find_all("dd", class_="catalogue-metadata-description")
+                        birthplace = data[1].string
+                        found_artists[new_name] = found_artists.pop(name)
+                        print("Replacing " + name + " with " + new_name)
+                        find_gender(url, found_genders, gender, new_name)
+                        return birthplace
+                    except:
+                        find_gender(url, found_genders, gender, new_name)
+                        return "N/A"
+                else:
+                    gender.append("N/A")
+                    found_genders[new_name] = "N/A"
                     return "N/A"
-            else:
+            except:
                 gender.append("N/A")
+                found_genders[new_name] = "N/A"
                 return "N/A"
-        except:
-            gender.append("N/A")
-                return "N/A"
+        else:
+            find_gender(url, found_genders, gender, name)
+            return found_artists.get(new_name)
     else:
-        find_gender(url, found_genders, gender, name)
-        return found_artists.get(new_name)
+        gender.append("N/A")
+        found_genders[new_name] = "N/A"
+        return "N/A"
 
 
 def find_gender(url, found_genders, gender, name):
@@ -117,22 +126,22 @@ def find_gender(url, found_genders, gender, name):
             if error == []:   
                 data = soup.find_all("a", class_="link-block-target")
                 count = 0
-                num_tags = len(data)
                 for tag in data:
                     if "female vocalists" in tag.string:
-                        gender.append("F")
-                        found_genders[name] = "F"
-                        break
+                        count = 1
                     elif "male vocalists" in tag.string:
-                        gender.append("M")
-                        found_genders[name] = "M"
-                        break
-                    count += 1
-                    if count == num_tags:
-                        gender.append("N/A")
-                        found_genders[name] = "N/A"
+                        count = -1
                     else:
                         continue
+                if count == 1:
+                    gender.append("F")
+                    found_genders[name] = "F"
+                elif count == -1:
+                    gender.append("M")
+                    found_genders[name] = "M"
+                else:    
+                    gender.append("N/A")
+                    found_genders[name] = "N/A"
             else:
                 gender.append("N/A")
                 found_genders[name] = "N/A"
@@ -143,21 +152,23 @@ def find_gender(url, found_genders, gender, name):
         gender.append(found_genders.get(name))
 
 
-def add_to_csv(birthplaces, gender):
+def add_to_csv(birthplaces, gender, filename):
     """
     Args:
         birthplaces (dict): keys are artist names, values are birthplaces.
     """
-    dataset = pd.read_csv("official_charts_data_2.csv")
+    dataset = pd.read_csv(filename)
     dataset.insert(3, "Birthplace", birthplaces)
     dataset.insert(4, "Gender", gender)
-    dataset.to_csv("1971_2011_top_40.csv", index=False, encoding="utf-8")
-    print("Done!")
+    output = filename.replace("input", "output")
+    print(output)
+    dataset.to_csv(output, index=False, encoding="utf-8")
 
 
 def main():
-    dataset = "official_charts_data_2.csv"
+    dataset = "official_charts_data.csv"
     find_artist(dataset)
+    print("Done!")
 
 
 if __name__ == '__main__':
